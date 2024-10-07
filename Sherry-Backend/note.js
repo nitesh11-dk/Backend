ec8b22ee4f0c1baf3e34d321c87670785c3d8567
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-07 19:14:36 +0530

    till-25-done

---
+++ Sherry-Backend/note.js
@@ -0,0 +1,28 @@
+
+// *  CJS --> Common JS
+// * ESM --> EcmaScript Module
+
+
+
+// 24: Started 
+
+// Internet
+
+// ? IP Address
+//  Ipv4 
+//  Ipv6
+//  this ip is provided by DHCP Dynamic Host Configuration Protocol
+
+
+
+
+// MAC Address
+// Proxy
+// Reverse Proxy
+// VPN
+// Protocol
+// Server
+// Client
+// TCP
+// UDP
+
