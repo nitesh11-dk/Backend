f23769ca76faf520312dd5201797921288717e51
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-08 18:38:35 +0530

    till basics done

--- Sherry-Backend/note.js
+++ Sherry-Backend/note.js
@@ -4,25 +4,75 @@
 
 
 
-// 24: Started 
+// 24: to 35
 
 // Internet
 
 // ? IP Address
 //  Ipv4 
 //  Ipv6
-//  this ip is provided by DHCP Dynamic Host Configuration Protocol
+//  this ip is provided by DHCP Dynamic Host Configuration Protocol --given by isp (internet service provider)
+//  * it is itendity as globally 
+
+// ? MAC Address / Physical Address 
+//  * device ke pas physical adress hota hai yusko kehte hai mac adress 
+//  * it is itendity wan / wifi
+
+// ? Proxy
+//*   PROXY   jo hota hai vo client side par hota hai 
+// manlo A ko B se bat karna hai
+//  but humm A ko bolege bat karo C se aur then C ko bolge app B se bat karo 
+// it is like third person 
+//  , kavi kavi kuch website ko open karna allow nahi hota hai , so hum kisi third person ka use karte hai
+
+// ? Reverse Proxy
+//  Rev Proxy jo hota hai vo serverr side par set hoti hai 
+//  reqest acutal server pe jane se pahale , rev proxy par jata hai , yiske fhayade ye kii hai phalae vo request ko check karega ,kya koi mallesious data to nahi hai ya ye request bar bar to nahi arra hai like for dedos attack and rate limiting the request 
 
 
 
+// ? VPN ( Virtual Private Network)
+//  lly proxy jese hota hai jese ki A --> B , then A-->C then after C --> B
+//   difference hai in proxy vo vhi data  hum transfer karte hai , vo encrypted nahi hota hai ,but it can be tracked , and be directly can be read 
+// but the VPN , the data is encrypted and also it cannot be directly read 
 
-// MAC Address
-// Proxy
-// Reverse Proxy
-// VPN
-// Protocol
-// Server
+
+// ? Server
+// server hai computer system  hai jo kii  programmed ho request ko accecpt karne ke liye aur response ko bhejne ke liye  , 
+//  it should connected to internet , and can share and accpet data or files 
+
 // Client
-// TCP
-// UDP
+
+
+// ?  Protocol
+//  yak standard set of rules , that is called as protocol
+
+//  ? ISP (Internet Service Provider)
+
+// *  TCP: Transmission Control Protocol.
+// reqest bhejne ke bad kisi ko  , yuska  accknowledge milna chahiye , milta hii hai in  TCP IP me  
+//  IP --> Internet Protocal (DHCP)
+//  alag alag time par  do logo ka same ip adress ho sakta hai 
+// yak yaise protocol , yak ip adress pe app data bhejte hai aur yuska acknowledge milta hai kii data pahucha kii nahi pachucha then app dusara packet ya data behjte ho 
+//  tcp make sure the data is reached to the other person , bits slower 
+
+//  ? UDP
+// yee yaise protocol call jo data bhejte rahta hai yusko farak ya cheeck nahi karta kii data pahucha kii nahi pahucha ,indepeennt of acknowledge of the  other side , example are straming services 
+//  udp ka kama hai ,jaldi se jaldi data dikhana  
+
+
+//  HTTP :
+
+// V1  
+//   conjection
+// app data to baut teji se bhej  rahe ho  router par , but roter dhere dhere data ko transmitt kar rahah hai 
+
+//  V2 
+// kafi jada data bhejte ho , ajj  ke time ke hisab hai v2 vhi slow hai vut better hai v1 se 
+
+//  v3 
+// V3 has , TCPIP ,UDP
+
+
+
 
