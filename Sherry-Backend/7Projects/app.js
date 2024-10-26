b18e02c8d4019c4de1fa6a9ef39c9e2b36b1c671
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-26 09:28:25 +0530

    50% project done

--- Sherry-Backend/7Projects/app.js
+++ Sherry-Backend/7Projects/app.js
@@ -14,19 +14,64 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    fs.readdir('./files', (err, files) => {
+        if (err) return res.send("Error reading files.");
+        res.render("index.ejs", { files });
+    });
+})
+
+
 app.get('/create', (req, res) => {
- 
-    const today = new Date();
-  const fn = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
-  
+  const today = new Date();
+const fn = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
 
-  fs.writeFile(`./files/${fn}`, 'hello', (err) => {
-    if (err) return res.send("Error creating the file.");
-    res.send("File created and watching.");
-  });
-
-
+fs.writeFile(`./files/${fn}`, 'hello', (err) => {
+  if (err) return res.send("Error creating the file.");
+  res.redirect("/");
 });
+});
+
+
+app.get("/edit/:filename", (req, res) => {
+  const  filename = req.params.filename;  
+  fs.readFile(`./files/${filename}`, 'utf8', (err, data) => {
+    if (err) return res.send("Error reading file.");
+    res.render("edit.ejs", { filename, data });
+  });
+})
+
+app.post("/update/:filename", (req, res) => {
+  const filename = req.params.filename;   
+  fs.writeFile(`./files/${filename}`, req.body.filedata, (err) => {
+  if (err) return res.send("Error updating file.");
+  });
+  res.redirect("/");
+})
+
+app.get('/view/:filename', (req, res) => {
+  const filename = req.params.filename;
+  fs.readFile(`./files/${filename}`, 'utf8', (err, data) => {
+      if (err) return res.send("Error reading file.");
+      res.render("view.ejs", { filename, data });
+  });
+});
+
+app.get('/delete/:filename', (req, res) => {
+  const filename = req.params.filename;
+  fs.unlink(`./files/${filename}`, (err) => {
+    if (err) return res.send("Error deleting file.");
+    res.redirect("/");
+  });
+});
+
+
 
 
 
@@ -42,6 +87,6 @@ app.get('/create', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server started on port http://localhost:3000');
+app.listen(4000, () => {
+    console.log('Server started on port http://localhost:4000');
 });
