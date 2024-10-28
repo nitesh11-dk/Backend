b8e0026ec61c5c39d63eab0d910a58002583edb9
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-28 16:47:59 +0530

    alldone

--- Delta-Backend/7_SQL_WITH_NODE/app.js
+++ Delta-Backend/7_SQL_WITH_NODE/app.js
@@ -18,35 +18,122 @@ connection.connect((err) => {
 });
 
 
-
+//  function to run sql command 
 function runsql(sqlcommand){
-connection.query(sqlcommand, (error, results, fields) => {
-    if (error) throw error;
-    console.log('Data: ', results);
-    connection.end();
-})
+    connection.query(sqlcommand, (error, results, fields) => {
+        if (error) throw error;
+        console.log('Data: ', results);
+        connection.end();
+    })
 }
 
 
-// runsql("SELECT *  FROM users")
-
-// ? place holderrs 
-
-let query = 'INSERT INTO users (id, username,password , email) VALUES (?, ?,? ,? )';
-let arr = [20, 'john', 'johndoe', 'johndoe123'];
-let arr23 = [createRandomUser(),createRandomUser(),createRandomUser(),createRandomUser()];
-
-// console.log(arr23);
-
+    
+//  funciton that will crate some demo data  
 function createRandomUser() {
     return [
        faker.string.uuid(),
        faker.internet.username(), 
        faker.internet.email(),
-       faker.image.avatar(),
        faker.internet.password(),
     ];
   }
+
+
+// ? normal inseting data into tables 
+// let query = `INSERT INTO users (id, username, email, password) VALUES (1223,"nitesh23","nitesh@123","123456")`;
+
+// connection.query(query, (error, results, fields) => {
+//     if (error) throw error;
+//     console.log('Data: ', results);
+//     connection.end();
+// })
+
+//  OR , coustom fucntion 
+// runsql(query)
+
+//   runsql("SELECT *  FROM users")
+
+//  ? using faker to insert data
+// let query = `INSERT INTO users (id, username, email, password) VALUES ('${faker.string.uuid()}', '${faker.internet.username()}', '${faker.internet.email()}', '${faker.internet.password()}')`;
+
+// runsql(query)
+//   runsql("TRUNCATE TABLE users ")
+
+// ?  using placeholders 
+
+// ?  single data
+// let query = `INSERT INTO users (id, username, email, password) VALUES (?,?,?,?)`;
+// let placeeholder = [1212, 'nitesh23', 'nitesh@123', '123456'];
+// connection.query(query,placeeholder ,  (error, results, fields) => {
+//         if (error) throw error;
+//         console.log('Data: ', results);
+//         connection.end();
+//     })
+
+// ? Multiple values 
+
+//  VALID 
+// let query = `INSERT INTO users (id, username, email, password) VALUES ?`;
+// let placeeholder = [[1212, 'nitesh23', 'nitesh@123', '123456']];
+// connection.query(query,[placeeholder] ,  (error, results, fields) => {
+//         if (error) throw error;
+//         console.log('Data: ', results);
+//         connection.end();
+//     })
+
+// ? VALID 
+// let query = `INSERT INTO users (id, username, email, password) VALUES ?`;
+// let placeeholder = [[[1212, 'nitesh23', 'nitesh@123', '123456']]];
+// connection.query(query,placeeholder ,  (error, results, fields) => {
+//         if (error) throw error;
+//         console.log('Data: ', results);
+//         connection.end();
+//     })
+
+
+// ? in more  than one
+let query = `INSERT INTO users (id, username, email, password) VALUES ?`;
+let placeeholderarr = [
+    [1212, 'nitesh23', 'nitesh@123', '123456'],
+[12342, 'nitesh2ssfd3', 'ndsfsitesh@123', '123456jkshdsfjkh']
+];
+// connection.query(query,[placeeholderarr] ,  (error, results, fields) => {
+//         if (error) throw error;
+//         console.log('Data: ', results);
+//         connection.end();
+//     })
+
+// ? OR USE coustom function :
+
+//  function to run sql command with placeholders
+function runSQL(sqlcommand , placeholders){
+    connection.query(sqlcommand,[placeholders] ,  (error, results, fields) => {
+        if (error) throw error;
+        console.log('Data: ', results);
+        connection.end();
+    })
+    }
+
+    let data = [] ;
+
+    for (let i = 0; i < 200; i++) {
+        data.push(createRandomUser());
+    }
+    // console.log(data);
+    
+    runSQL(query,data)
+    
+ 
+    
+
+
+
+
+//   runsql("TRUNCATE TABLE users ")
+//   runsql("SELECT *  FROM users ")
+
+
 
 
 
