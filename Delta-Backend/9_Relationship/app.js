bc8421828b0e31a3cecd6ef46d66bb931539de38
Nitesh-DK <nittesh.dk@gmail.com>
2024-11-01 09:04:48 +0530

    relationship done

---
+++ Delta-Backend/9_Relationship/app.js
@@ -0,0 +1,23 @@
+// Relationships
+// SQL (via Foreign Keys)
+// • one to one
+// • one to many
+// • many to many
+
+//  cardinality 
+
+// Mongo Relationships
+// One to Many I Approach 1 (one to few)
+// Store the child document inside parent
+// _id: ObjectldC651d1b116976164a9cbf552ff'),
+// • sherlockholmes• ,
+// username :
+// addresses: t
+// J,
+// v:
+// location: '221B Baker Street'
+// , city:
+// ' London '
+// location: • P36 DownTown•, city:
+// • London'
+// 1 
